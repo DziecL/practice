4bcea67549c1f510d4aecf53920a0d2867ddabfc
bestboy2021 <80074008+bestboy2021@users.noreply.github.com>
2021-03-06 19:47:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import pandas as pd
 import plotly.express as px
-import shutil
 from datetime import datetime
-import time
 
 url = 'http://api.open-notify.org/iss-now.json'
 
 df = pd.read_json(url)
-
 
 df['latitude'] = df.loc['latitude', 'iss_position']
 df['longitude'] = df.loc['longitude', 'iss_position']
@@ -21,11 +18,4 @@ fig = px.scatter_geo(df, lat = 'latitude', lon = 'longitude')
 now = datetime.now()
 date = str(now.strftime("%d-%m-%Y %H-%M-%S"))
 
-fig.write_html('first_figure ' + date + '.html', auto_open=True)
-
-time.sleep(1)
-
-src = 'C:/Users/User/PycharmProjects/ISS/first_figure ' + date + '.html'
-dest = "C:/Users/User/PycharmProjects/ISS/arch/first_figure " + date + ".html"
-
-shutil.move(str(src), str(dest))
+fig.write_html('C:/Users/User/PycharmProjects/ISS/arch/first_figure ' + date + '.html', auto_open=True)
