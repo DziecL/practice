26cc6d4cb84d0daf107e124ac423615b84ee49bc
bestboy2021 <80074008+bestboy2021@users.noreply.github.com>
2021-03-23 01:12:42 +0100

    Update Caesar_cipher.py

--- Caesar_cipher.py
+++ Caesar_cipher.py
@@ -36,3 +36,16 @@ def decode2(text, seed, seed2):
     d = [chr(ord(i) - random.randint(1, seed2)) for i in text]
     return "".join(d)
 
+# additional layer of protection could be added, another seed for the range of unicode characters ('33' and '1100' in the loop, both or any of them), 
+# it should loop on itself so that you can input a large number and you won't get an error and you won't get all the weird characters
+# probably overly complicated but with up to 4 variables seems decent enough
+
+uni = []
+for i in range(33, 1100):
+    uni.append(chr(i)) if (i <= 126 or i >= 151) else print(chr(i))
+print(uni)
+
+a = 10001
+while a > len(uni):
+    a -= len(uni)
+print(f'{uni[a]} {str(a)}')
